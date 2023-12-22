098692c6c7f4fc9243e70599005b0a32b0334729
Skylar Thompson <skylar2@uw.edu>
2023-12-22 10:45:18 -0800

    remove validation from network::mroute::validate_gw re #1

--- manifests/mroute/validate_gw.pp
+++ manifests/mroute/validate_gw.pp
@@ -1,6 +1,7 @@
 # GWs validator. We need it to have compatibility with puppet 3.x, 'cause it doesn't support each.
 #
-define network::mroute::validate_gw($routes) {
+define network::mroute::validate_gw(
+  Hash[String,String] $routes
+) {
   $route = $routes[$name]
-  validate_string($route)
 }
